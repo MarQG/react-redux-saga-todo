013a29ad692fd54901c7a17054daa907860844b1
Ferenc Gutierrez <ferenc.g87@gmail.com>
2018-09-18 22:21:00 -0500

    Created MainPage

---
+++ todo-list/src/pages/MainPage.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Grid, Paper } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+	root: {
+		flexGrow: 1,
+		backgroundColor: '#eee',
+		height: '100vh'
+	},
+	control:{
+		padding: theme.spacing.unit * 2
+	},
+	paper: {
+		height: '100px',
+		width: '200px',
+		padding: theme.spacing.unit * 3
+	}
+});
+
+export const MainPage = ({classes}) => (
+	<Grid container className={classes.root} spacing={16}>
+		<Grid item className={classes.control} xs={12}>
+			<Grid container justify="center">
+				<Paper className={classes.paper}>
+					<h1>To-do List</h1>
+				</Paper>
+			</Grid>
+		</Grid>
+	</Grid>
+);
+
+MainPage.propTypes = {
+	classes: PropTypes.object.isRequired
+}
+
+export default withStyles(styles)(MainPage);
